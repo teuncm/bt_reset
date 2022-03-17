2f17c23cc651d4148cccb9d2f9f40f52714e7cca
teuncm <teun.c.mathijssen@gmail.com>
2022-03-17 13:52:06 +0100

    Added reset script

---
+++ bt_reset.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Remove all connected external devices
+for device in $(bluetoothctl devices | grep -o "[[:xdigit:]:]\{8,17\}"); do
+    echo $(bluetoothctl remove $device)
+done
+
+# Re-insert into kernel
+sudo modprobe -rw btusb
+sudo modprobe btusb
+
+# Re-enable device itself
+rfkill block bluetooth
+rfkill unblock bluetooth
+
+# Restart service
+systemctl restart bluetooth
